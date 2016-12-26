a3ea75263eb7c0eeba04d8c91abd6d9ae06a5bf1
chggri <chenguangrui@163.com>
2016-12-26 12:52:31 +0800

    Initial commit

---
+++ 2017/js/comm.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Ibokan on 2015/10/13.
+ */
+function _pages(){
+    var _dw = document.documentElement.clientWidth,_sc = _dw/640;
+    document.body.style.zoom = _sc;
+}
+window.onresize = function(){
+    _pages();
+}
+window.onload = function(){
+    _pages();
+}
